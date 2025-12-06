e47fc6618cad2a01b2facfce3536f81bb742eeed
James Greenaway <james@greenaway.io>
2025-12-06 18:57:58 +0000

    use vercel adaptor for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import react from '@astrojs/react'
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+    devImageService: 'sharp',
+  }),
 })
